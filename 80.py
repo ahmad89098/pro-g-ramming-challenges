849fe7ce6349d6ae0dbe3202d0c7f692abe76a5e
ahmad89098 <ahmad89098@gmail.com>
2019-06-25 11:21:59 +0500

    Add files via upload

---
+++ 80.py
@@ -0,0 +1,10 @@
+#Pro/g/ramming Challenges
+#80: Factorial
+#by ahmad89098
+
+factorial = 1
+number = input("Enter your number: ")
+
+for i in range (1,int(number)+1):
+     factorial = factorial * i
+print(factorial)
