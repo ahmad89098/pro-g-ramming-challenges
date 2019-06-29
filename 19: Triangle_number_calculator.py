854162f6a692e7213dfbbe665f767b63b548ba3f
ahmad89098 <ahmad89098@gmail.com>
2019-06-29 17:53:20 +0500

    Create 19: Triangle_number_calculator.py

---
+++ 19: Triangle_number_calculator.py
@@ -0,0 +1,8 @@
+# Pro/g/ramming Challenges
+# 19: Triangle number calculator
+# by ahmad89098
+
+# The formula for calculating triangle numbers is n(n+1)/2
+
+n = input("Enter any number")
+print(((n*n) + n)/2)
